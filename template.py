f639dc1e63728f3c4c55ba197aebd4d5dc37ce67
owenarsene <118958358+owenarsene@users.noreply.github.com>
2023-07-01 20:15:32 +0800

    Create template.py

---
+++ template.py
@@ -0,0 +1,11 @@
+namespace as project USC
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  print('Hello, world\n"):
+        ^!
+        return 0:
+}
+  automobile_system for researching area
+from 
